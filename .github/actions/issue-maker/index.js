f4fe351aa24ac765fe923d40b225cad699f46573
codingChewie <codingChewie@gmail.com>
2021-07-19 13:31:47 -0500

    updated

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -14,10 +14,6 @@ async function run() {
       title: issueTitle,
       body: jokeBody,
     });
-    console.log("repo:", github.context.repo.repo);
-    console.log("owner:", github.context.repo.owner);
-    console.log("title:", issueTitle);
-    console.log("body:", jokeBody);
   } catch (err) {
     core.setFailed(err.message);
   }
